a352fe470037f383b5a34aa84506477b858868dc
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-03-28 13:48:07 +0200

    feat: remade folder structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,17 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      title: 'Frontend Mentor | Space tourism website',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'Responsive space tourism website with Tailwind' },
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  }
 })
