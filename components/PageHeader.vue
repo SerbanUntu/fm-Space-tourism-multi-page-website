5b736fcc564daa4ecb69080ed01b1c61a869e691
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-04-03 08:30:17 +0300

    refactor: header dynamic links with regex

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,6 +1,12 @@
 <script setup>
 const route = useRoute()
 const showMenu = ref(false)
+const entries = [
+  { link: '/', regex: '^/$' },
+  { link: '/destination', regex: '^/destination' },
+  { link: '/crew', regex: '^/crew' },
+  { link: '/technology', regex: '^/technology' },
+]
 </script>
 <template>
   <section class="flex flex-row ml-[3vw] mr-[3vw] md:mr-0 relative top-[3vh] md:top-0 xl:top-[3vh] items-center">
@@ -11,28 +17,10 @@ const showMenu = ref(false)
       <Line class="hidden xl:block" />
       <nav
         class="flex-row h-[96px] hidden md:flex md:w-[450px] xl:w-[830px] bg-header backdrop-blur-[32px] justify-between md:px-10  xl:px-20 ml-[-30px] shrink-0">
-        <NuxtLink class="flex flex-row w-fit h-full items-center border-white border-b-[3px] border-opacity-0" to="/"
-          :class="{ 'hover:border-opacity-50': route.path !== '/', 'border-opacity-100': route.path === '/' }">
-          <strong class="hidden w-[29px] xl:block">00</strong>
-          <p class="uppercase">Home</p>
-        </NuxtLink>
-        <NuxtLink class="flex flex-row w-fit h-full items-center border-white border-b-[3px] border-opacity-0"
-          to="/destination/moon"
-          :class="{ 'hover:border-opacity-50': !route.path.includes('/destination'), 'border-opacity-100': route.path.startsWith('/destination') }">
-          <strong class="hidden w-[29px] xl:block">01</strong>
-          <p class="uppercase">Destination</p>
-        </NuxtLink>
-        <NuxtLink class="flex flex-row w-fit h-full items-center border-white border-b-[3px] border-opacity-0"
-          to="/crew/commander"
-          :class="{ 'hover:border-opacity-50': !route.path.includes('/crew'), 'border-opacity-100': route.path.startsWith('/crew') }">
-          <strong class="hidden w-[29px] xl:block">02</strong>
-          <p class="uppercase">Crew</p>
-        </NuxtLink>
-        <NuxtLink class="flex flex-row w-fit h-full items-center border-white border-b-[3px] border-opacity-0"
-          to="/technology/vehicle"
-          :class="{ 'hover:border-opacity-50': !route.path.includes('/technology'), 'border-opacity-100': route.path.startsWith('/technology') }">
-          <strong class="hidden w-[29px] xl:block">03</strong>
-          <p class="uppercase">Technology</p>
+        <NuxtLink v-for="(entry, index) in entries" class="flex flex-row w-fit h-full items-center border-white border-b-[3px] border-opacity-0" :to="entry.link"
+          :class="{ 'hover:border-opacity-50': !route.path.match(entry.regex), 'border-opacity-100': route.path.match(entry.regex) }">
+          <strong class="hidden w-[29px] xl:block">{{ '0' + index.toString() }}</strong>
+          <p class="uppercase">{{ entry.link === '/' ? 'Home' : entry.link.slice(1) }}</p>
         </NuxtLink>
       </nav>
     </article>
@@ -43,30 +31,11 @@ const showMenu = ref(false)
   <article
     class="flex md:hidden absolute right-0 top-0 h-screen bg-header backdrop-blur-[32px] w-[250px] transition-all duration-200 z-20 p-10 pt-28"
     :class="{'translate-x-[250px]': !showMenu}">
-    <nav
-      class="flex flex-col gap-4">
-      <NuxtLink class="flex flex-row h-fit w-fit items-center border-white border-b-[3px] border-opacity-0" to="/"
-        :class="{ 'hover:border-opacity-50': route.path !== '/', 'border-opacity-100': route.path === '/' }">
-        <strong class="block md:hidden w-[29px] xl:block">00</strong>
-        <p class="uppercase">Home</p>
-      </NuxtLink>
-      <NuxtLink class="flex flex-row h-fit w-fit items-center border-white border-b-[3px] border-opacity-0"
-        to="/destination/moon"
-        :class="{ 'hover:border-opacity-50': !route.path.includes('/destination'), 'border-opacity-100': route.path.startsWith('/destination') }">
-        <strong class="block md:hidden w-[29px] xl:block">01</strong>
-        <p class="uppercase">Destination</p>
-      </NuxtLink>
-      <NuxtLink class="flex flex-row h-fit w-fit items-center border-white border-b-[3px] border-opacity-0"
-        to="/crew/commander"
-        :class="{ 'hover:border-opacity-50': !route.path.includes('/crew'), 'border-opacity-100': route.path.startsWith('/crew') }">
-        <strong class="block md:hidden w-[29px] xl:block">02</strong>
-        <p class="uppercase">Crew</p>
-      </NuxtLink>
-      <NuxtLink class="flex flex-row h-fit w-fit items-center border-white border-b-[3px] border-opacity-0"
-        to="/technology/vehicle"
-        :class="{ 'hover:border-opacity-50': !route.path.includes('/technology'), 'border-opacity-100': route.path.startsWith('/technology') }">
-        <strong class="block md:hidden w-[29px] xl:block">03</strong>
-        <p class="uppercase">Technology</p>
+    <nav class="flex flex-col gap-4">
+      <NuxtLink v-for="(entry, index) in entries" class="flex flex-row h-fit w-fit items-center border-white border-b-[3px] border-opacity-0" :to="entry.link"
+        :class="{ 'hover:border-opacity-50': !route.path.match(entry.regex), 'border-opacity-100': route.path.match(entry.regex) }">
+        <strong class="w-[29px]">{{ '0' + index.toString() }}</strong>
+        <p class="uppercase">{{ entry.link === '/' ? 'Home' : entry.link.slice(1) }}</p>
       </NuxtLink>
     </nav>
     <button class="absolute top-[5vh] right-[3vw]" @click="showMenu = false">
